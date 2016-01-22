f50edc136ccf5ca6745102091276002e487f41cc
Vagner Cazarotto <vagnercazarotto@gmail.com>
2016-01-22 20:55:33 -0200

    Add funcitions to Just Java

--- JustJava/app/src/main/java/com/tkcode/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/tkcode/justjava/MainActivity.java
@@ -1,13 +1,54 @@
 package com.tkcode.justjava;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.TextView;
+import java.text.NumberFormat;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends ActionBarActivity {
+    private int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        display_value(quantity * 5);
+    }
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    private void display_value(double number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    ///////////////// OK,now i'll create function for increment and decrement
+    public void incrementButton(View view){
+        quantity = quantity + 1;
+        display(quantity);
+    }
+
+    public void decrementButton(View view){
+        if(quantity > 0){
+            quantity = quantity - 1;
+            display(quantity);
+        }
+    }
+
 }
