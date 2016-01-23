b75085a6db68b49a19a973f80bec76a79dd4e683
Vagner Cazarotto <vagnercazarotto@gmail.com>
2016-01-23 13:23:53 -0200

    update for team A

--- PracticeSet2/app/src/main/java/com/tkcode/practiceset2/MainActivity.java
+++ PracticeSet2/app/src/main/java/com/tkcode/practiceset2/MainActivity.java
@@ -2,12 +2,66 @@ package com.tkcode.practiceset2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+     private int scoreTeamA = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public boolean onCreateOptionsMen(Menu menu) {
+        // This add items to the action bar if it is present
+         //  getMenuInflater().inflate(R.menu,menu_main, menu);
+        return true;
+    }
+
+
+
+    public boolean onOptionsSelected(MenuItem item){
+        // handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml
+        int id = item.getItemId();
+
+        // no inspection simplifiableIfStatment
+        //if (id == R.id.action_setting) {
+         //   return true;
+        //}
+        return super.onContextItemSelected(item);
+        }
+
+
+        // Displays the given score for team A
+    public void displayForTeamA (int score){
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void addThreeForTeamA(View v){
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void addTwoForTeamA(View v){
+        scoreTeamA = scoreTeamA + 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void addOneForTeamA(View v){
+        scoreTeamA = scoreTeamA + 1;
+        displayForTeamA(scoreTeamA);
+    }
+
+
+
 }
