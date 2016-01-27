aaff264474214b84f7a71f105b2a9f9493ee511f
Vagner Cazarotto <vagnercazarotto@gmail.com>
2016-01-27 10:11:24 -0200

    Add a Intent to open the user local preference in the MAP app

--- Sunshine-Version-2/app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ Sunshine-Version-2/app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.android.sunshine.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    /////// Start global variables
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,10 +50,39 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
 
+        //// conditions to call the Intent MAP
+        if (id == R.id.action_map){
+            openPreferredLocationInMap();
+            return true;
+        }
+
+
         return super.onOptionsItemSelected(item);
     }
 
+    ////////////////////////////////////////
+    /// new method for call open preferred location in map
+    private void openPreferredLocationInMap(){
+        //// Read the user preferences
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String location = sharedPrefs.getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
 
+        //// We should build a link to invoke the location intent, reference: http://developer.android.com/guide/components/intents-common.html#Maps
+        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon().appendQueryParameter("q",location).build();
+
+        //// Then we create a view intent, indicating its location in the data URI
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+
+        //// we need to start a Activity, but only do this if the Activity resolves successfully
+        if (intent.resolveActivity(getPackageManager())!= null ){
+            startActivity(intent);
+        }  else {
+            Log.d(LOG_TAG,"Couldn't call " + location + ", no receiving apps installed!!");
+        }
+    }
+
+    ////////////////////////////////////////
 }
 
 
