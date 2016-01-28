6e90f3f439bf31bd5165707c0dafff25a0f89d74
Vagner Cazarotto <vagnercazarotto@gmail.com>
2016-01-28 16:26:21 -0200

    Add a CursorAdapter with ListView and now go back to our original project

--- DictionaryProviderExample/app/src/main/java/android/example/com/dictionaryproviderexample/MainActivity.java
+++ DictionaryProviderExample/app/src/main/java/android/example/com/dictionaryproviderexample/MainActivity.java
@@ -20,21 +20,36 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.UserDictionary.Words;
 import android.support.v7.app.ActionBarActivity;
-import android.widget.TextView;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
 
 /**
  * This is the central activity for the Provider Dictionary Example App. The purpose of this app is
  * to show an example of accessing the {@link Words} list via its' Content Provider.
  */
 public class MainActivity extends ActionBarActivity {
+    ////////////////////////////////////////////////
+    private static final String[] COLUMNS_TO_BE_BOUND = new String[]{
+            Words.WORD,
+            Words.FREQUENCY
+    };
+
+    private static final int[] LAYOUT_ITEMS_TO_FILL = new int[]{
+            android.R.id.text1,
+            android.R.id.text2
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //////////////////////////////////
-        /// Get the TextView whith will be populated with the Dictionary ContentProvider data.
-        TextView dictTextView = (TextView) findViewById(R.id.dictionary_text_view);
+        /// Get the TextView which will be populated with the Dictionary ContentProvider data.
+        /// Alter the TextView to ListView for work with Cursor Adapter
+        ListView dictListView = (ListView) findViewById(R.id.dictionary_list_view);
 
         /// Get the ContentResolver which will send a message to the ContentProvider
         ContentResolver resolver = getContentResolver();
@@ -42,31 +57,28 @@ public class MainActivity extends ActionBarActivity {
         /// Get a Cursor containing all of the rows in the Words table
         Cursor cursor = resolver.query(Words.CONTENT_URI,null,null,null,null);
 
-        //////////////////////////////////
-        /// Now use the Cursor to insert into TextView
+        /// Activate for debug
+        Log.e("TEST WITH CONTENT URI:", Words.CONTENT_URI.toString());
 
-        ///Surround the cursor in a try statement so that the finally block will eventually execute
-        try{
-            dictTextView.setText("The UserDictionary contais ");
+        /////////////////////////////////////////////////////////////
+        //// About Adapter:
+        //// The SimpleCursorAdapter needs the following arguments:
+        //// What layout xml each list item will use,  for simplicity use android.R.layout.two_line_list_item which is a built-in layout.
+        //// The Cursor, The columns in the Cursor to use (their String headings), stored in a String array.
+        //// The id’s of text views contained in the layout that you passed as the first parameter, stored as an int array.
+        //// And an optional settings flag. Just set it to 0 for now.
+        //// http://developer.android.com/guide/topics/ui/declaring-layout.html#AdapterViews
+        
 
+        /// changed the min SKD version for compatibility, now min is 11.
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+                android.R.layout.two_line_list_item,
+                cursor,
+                COLUMNS_TO_BE_BOUND,
+                LAYOUT_ITEMS_TO_FILL,
+                0);
 
-            /// Get the index of the column containing the actual words, using
-            /// UserDictionary.Words.WORD, which is the header of the word column.
-            int wordColumn = cursor.getColumnIndex(Words.WORD);
-
-            /// Iterates through all returned rows in the cursor
-
-            while (cursor.moveToNext()){
-                /// Use that index to extract the String value of the word
-                /// at the current row the cursor is on.
-                String word = cursor.getString(wordColumn);
-                dictTextView.append(("\n" + word));
-            }
-        } finally {
-            /// Remember always close your cursor to avoid memory leaks
-            cursor.close();
-        }
-
-
+        // Attach the adapter to the ListView
+        dictListView.setAdapter(adapter);
     }
 }
