9018af283194ab8b70856c0faedd58c1559ad7db
Vagner Cazarotto <vagnercazarotto@gmail.com>
2016-01-31 15:36:45 -0200

    Making your ContentProvider Accessible

--- Sunshine-Version-3/app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ Sunshine-Version-3/app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -155,13 +155,6 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
         weatherTask.execute(location);
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        updateWeather();
-    }
-
-
     /// Add new functions
 
     @Override
@@ -201,5 +194,9 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
     }
 
 
+    void onLocationChanged(){
+        updateWeather();
+        getLoaderManager().restartLoader(FORECAST_LOADER,null,this);
+    }
 
 }
