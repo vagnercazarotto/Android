02a24368b6f57782a058d281f0198b0977a0a6b9
Vagner Cazarotto <vagnercazarotto@gmail.com>
2016-02-01 15:06:13 -0200

    Add an paralel project (Super Mario Bros with LibGDX)

---
+++ Mario/desktop/src/com/vagnercazarotto/mariobros/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.vagnercazarotto.mariobros.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.vagnercazarotto.mariobros.MarioBros;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MarioBros(), config);
+	}
+}
