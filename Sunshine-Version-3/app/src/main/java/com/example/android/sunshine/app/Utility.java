89992a69068b0110cce786e871d089b0be7b5551
Vagner Cazarotto <vagnercazarotto@gmail.com>
2016-02-04 14:24:28 -0200

    SUNSHINE: Fix, Sync data

--- Sunshine-Version-3/app/src/main/java/com/example/android/sunshine/app/Utility.java
+++ Sunshine-Version-3/app/src/main/java/com/example/android/sunshine/app/Utility.java
@@ -23,14 +23,11 @@ public class Utility {
                 .equals(context.getString(R.string.pref_units_metric));
     }
 
-    static String formatTemperature(Context context,double temperature, boolean isMetric) {
-        double temp;
-        if ( !isMetric ) {
-            temp = 9*temperature/5+32;
-        } else {
-            temp = temperature;
-        }
-        return context.getString(R.string.format_temperature, temp);
+    public static String formatTemperature(Context context,double temperature) {
+        String suffix = "\u00B0";
+        if (!isMetric(context))
+            temperature = (temperature * 1.8) + 32;
+            return String.format(context.getString(R.string.format_temperature), temperature);
     }
 
     static String formatDate(long dateInMilliseconds) {
