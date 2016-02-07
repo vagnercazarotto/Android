0319fc25ce278d1166236df404e3d3b0c2ab20c5
Vagner Cazarotto <vagnercazarotto@gmail.com>
2016-02-07 09:13:09 -0200

    Android App Lifecycle

--- Intermediate/Recap/app/src/main/java/com/tkcode/recap/MainActivity.java
+++ Intermediate/Recap/app/src/main/java/com/tkcode/recap/MainActivity.java
@@ -1,7 +1,10 @@
 package com.tkcode.recap;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("Lifecycle","onCreate called");
+    }
+
+    protected void onStart(){
+        super.onStart();
+        Log.i("Lifecycle","onStart called");
+    }
+
+    protected void onResume(){
+        super.onResume();
+        Log.i("Lifecycle", "onResume called");
+    }
+
+    protected void onPause(){
+        super.onPause();
+        Log.i("Lifecycle", "onPause called");
+    }
+
+    protected void onStop(){
+        super.onStop();
+        Log.i("Lifecycle", "onStop called");
+    }
+
+    protected void onRestart(){
+        super.onRestart();
+        Log.i("Lifecycle", "onRestart called");
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i("Lifecycle", "onDestroy called");
+    }
+
+
+
+
+    public void onButtonTouch(View view){
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText("You Touched in the Button");
     }
 }
