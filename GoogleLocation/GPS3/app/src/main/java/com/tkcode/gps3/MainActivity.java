47a99769a4d834106a4b34ba8ff94113ed8861b0
Vagner Cazarotto <vagnercazarotto@gmail.com>
2016-02-12 11:55:31 -0200

    GPS3 Implement Button

--- GoogleLocation/GPS3/app/src/main/java/com/tkcode/gps3/MainActivity.java
+++ GoogleLocation/GPS3/app/src/main/java/com/tkcode/gps3/MainActivity.java
@@ -1,5 +1,6 @@
 package com.tkcode.gps3;
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -7,19 +8,24 @@ import android.content.IntentFilter;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.ActivityRecognition;
 import com.google.android.gms.location.DetectedActivity;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener {
+public class MainActivity extends ActionBarActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, ResultCallback<Status> {
 
     // Create fix variables
     protected ActivityDetectionBroadcastReceiver mBroadcastReceiver;
@@ -27,6 +33,17 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     private TextView mStatusText;
     private Button requestUpdatesButton;
     private Button removeUpdatesButton;
+    protected static final String TAG="activity";
+
+
+    public void onResult(Status status){
+        if (status.isSuccess()){
+            Log.e(TAG,"SUCCESSFULLY ADD ACTIVITY DETECTION");
+        } else {
+            Log.e(TAG,"ERROR ADDING OR REMOVING ACTIVITY DETECTION" + status.getStatusMessage());
+        }
+    }
+
 
     @Override
     protected void onStart() {
@@ -49,7 +66,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     @Override
     protected void onResume() {
-        // registre the broadcast receiver that informs this activity 
+        // registre the broadcast receiver that informs this activity
         super.onResume();
         LocalBroadcastManager.getInstance(this).registerReceiver(mBroadcastReceiver,
                 new IntentFilter(Constants.BROADCAST_ACTION));
@@ -129,6 +146,43 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
             }
             mStatusText.setText(strStatus);
         }
+    }
+
+
+
+    public void requestActivityUpdatesButtonHandler(View view){
+        // start check if google API is connected, if not send the message to user
+        if (!mGoogleApiClient.isConnected()){
+            Toast.makeText(this,getString(R.string.not_connected),Toast.LENGTH_SHORT).show();
+            return;
+        }
+        // Then start Activity
+        ActivityRecognition.ActivityRecognitionApi.requestActivityUpdates(mGoogleApiClient,
+                Constants.DETECTION_INTERVAL_IN_MILLISECONDS,
+                getActivityDetectionPendingIntent()).setResultCallback(this);
+        requestUpdatesButton.setEnabled(false);
+        removeUpdatesButton.setEnabled(true);
+    }
+
+    public void removeActivityUpdatesButtonHandler(View view){
+        // start check if google API is connected, if not send the message to user
+        if (!mGoogleApiClient.isConnected()){
+            Toast.makeText(this,getString(R.string.not_connected),Toast.LENGTH_SHORT).show();
+            return;
+        }
+        // Remove all activities for the PendingItent that was used
+        ActivityRecognition.ActivityRecognitionApi.removeActivityUpdates(mGoogleApiClient,
+                getActivityDetectionPendingIntent()).setResultCallback(this);
+        requestUpdatesButton.setEnabled(true);
+        removeUpdatesButton.setEnabled(false);
+    }
+
+    private PendingIntent getActivityDetectionPendingIntent() {
+        Intent intent = new Intent(this, DetectedActivitiesIntentService.class);
+
+        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when calling
+        // requestActivityUpdates() and removeActivityUpdates().
+        return PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 
 }
