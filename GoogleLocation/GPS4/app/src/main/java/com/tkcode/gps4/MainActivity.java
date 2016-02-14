62524fd17e248c137c8fe349b25f66d462bbd5f0
Vagner Cazarotto <vagnercazarotto@gmail.com>
2016-02-14 14:47:52 +0000

    GPS4: Add Geofence request and Pending Intent

--- GoogleLocation/GPS4/app/src/main/java/com/tkcode/gps4/MainActivity.java
+++ GoogleLocation/GPS4/app/src/main/java/com/tkcode/gps4/MainActivity.java
@@ -1,11 +1,14 @@
 package com.tkcode.gps4;
 
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingRequest;
 import com.google.android.gms.maps.model.LatLng;
 
 import java.util.ArrayList;
@@ -37,6 +40,27 @@ public class MainActivity extends AppCompatActivity {
                     .build());
         }
     }
+
+
+
+    // Create a geofence request
+    private GeofencingRequest getGeofencingRequest(){
+        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+
+        // only activate the geofance when the device is in the local of the geofence
+        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
+        // Add the Geofences to be monitored by Geofencing service.
+        builder.addGeofences(mGeofenceList);
+        // Return a GeofencingRequest
+        return builder.build(); 
+    }
+
+    private PendingIntent getPendingIntent(){
+        Intent intent = new Intent(this,GeofenceTransitionsIntentService.class);
+        // we use FLAG_UPDATE_CORRENT so that we get the same pending intent back when calling addGeofences()
+        return PendingIntent.getService(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
 
 
 
