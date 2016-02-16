b324a5ddf8e2f331a93f38244738b47a23bfc162
Vagner Cazarotto <vagnercazarotto@gmail.com>
2016-02-16 10:46:13 +0000

    Maps3: add StreetView

--- GoogleLocation/Maps3/app/src/main/java/com/tkcode/maps3/MainActivity.java
+++ GoogleLocation/Maps3/app/src/main/java/com/tkcode/maps3/MainActivity.java
@@ -3,11 +3,31 @@ package com.tkcode.maps3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
+import com.google.android.gms.maps.StreetViewPanorama;
+import com.google.android.gms.maps.StreetViewPanoramaFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.StreetViewPanoramaCamera;
+
+public class MainActivity extends AppCompatActivity implements OnStreetViewPanoramaReadyCallback {
+
+    private StreetViewPanoramaFragment streetViewPanoramaFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create a Street View panorama object
+        StreetViewPanoramaFragment streetViewPanoramaFragment = (StreetViewPanoramaFragment) getFragmentManager().findFragmentById(R.id.streetViewPanorama);
+        streetViewPanoramaFragment.getStreetViewPanoramaAsync(this);
+    }
+
+
+    @Override
+    public void onStreetViewPanoramaReady(StreetViewPanorama streetViewPanorama) {
+        streetViewPanorama.setPosition(new LatLng(36.0579667,-112.1430996));
+        StreetViewPanoramaCamera camera = new StreetViewPanoramaCamera.Builder().bearing(180).build();
+        streetViewPanorama.animateTo(camera,1000);
     }
 }
