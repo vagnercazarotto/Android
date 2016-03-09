76f0337445665f3bfeaf92985b43dcd5259fa4fe
Vagner Cazarotto <vagnercazarotto@gmail.com>
2016-03-09 21:08:54 +0000

    Custom Array Adapter middle - make action when is pressed

--- Intermediate/MyApplication2/app/src/main/java/tkcode/com/myapplication/MainActivity.java
+++ Intermediate/MyApplication2/app/src/main/java/tkcode/com/myapplication/MainActivity.java
@@ -2,9 +2,12 @@ package tkcode.com.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -19,6 +22,14 @@ public class MainActivity extends Activity {
         ListAdapter listAdapter = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,foods);
         ListView listView = (ListView) findViewById(R.id.listaItem);
         listView.setAdapter(listAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                            @Override
+                                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                                String food = String.valueOf(parent.getItemAtPosition(position));
+                                                Toast.makeText(MainActivity.this,food,Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
 
 
     }
