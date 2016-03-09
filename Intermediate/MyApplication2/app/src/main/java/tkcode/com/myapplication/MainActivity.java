55c44b5a08636aa1d37852b79db209081b15f6b1
Vagner Cazarotto <vagnercazarotto@gmail.com>
2016-03-09 20:59:49 +0000

    first step in a custom Array Adapter

--- Intermediate/MyApplication2/app/src/main/java/tkcode/com/myapplication/MainActivity.java
+++ Intermediate/MyApplication2/app/src/main/java/tkcode/com/myapplication/MainActivity.java
@@ -1,17 +1,26 @@
 package tkcode.com.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // A normal List Adapter
+        String[] foods = {"Bacon","Ham","Tuna","Candy","Meatball","Potato","Bacon","Ham","Tuna","Candy","Meatball","Potato"};
+        ListAdapter listAdapter = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,foods);
+        ListView listView = (ListView) findViewById(R.id.listaItem);
+        listView.setAdapter(listAdapter);
+
+
     }
 
 }
