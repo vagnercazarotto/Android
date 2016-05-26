d543c99f96603bf6e178a7048522c0e3fc98273f
Vagner <vagnercazarotto@gmail.com>
2016-05-25 21:00:49 -0300

    Parameterising Your Build

---
+++ Java/DataStructure/src/DataStructure/main.java
@@ -0,0 +1,11 @@
+package DataStructure;
+
+public class main {
+
+	public static void main(String[] args) {
+		ArrayDeque aD = new ArrayDeque(int.class);
+		// TODO Auto-generated method stub
+
+	}
+
+}
