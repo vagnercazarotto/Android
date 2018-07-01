714fd3029eba44bfcda62a215b74288f5910e40d
vagnercazarotto <vagnercazarotto@gmail.com>
2018-07-01 15:10:01 +0200

    Kotlin Variables

---
+++ Kotlin/kotlin Foundation/src/Variables.kt
@@ -0,0 +1,26 @@
+fun main(args: Array<String>) {
+
+    val name : String = "JonnyB"
+
+    var isAwesome = true
+    isAwesome = false
+
+
+    println("Is " + name + "awesome? " + isAwesome)
+
+    var a : Int = 3
+    var b = 6
+    println(a+b)
+
+    // mutable?
+
+    val doubleNum : Double = 123.45 //64 bit
+    val floatNum = 123.34f //23 bit
+
+    val longNum  = 321424343L // 64 bit
+
+    val aDouble = a.toString()
+
+    println(aDouble)
+
+}
