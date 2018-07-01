d4c01884f026b5c22e4f6f244eed032efc16ec01
vagnercazarotto <vagnercazarotto@gmail.com>
2018-07-01 16:01:38 +0200

    Conditional -> when !!

---
+++ Kotlin/kotlin Foundation/src/ConditionalLogic.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    val a = 2
+    val b = 2
+
+    if(a==b){
+        println("true!!")
+    }
+
+    val x = 2
+
+    when (x) {
+        1 -> println(" str + 1")
+        2 -> println(" str + 2")
+        else -> println(" x does not equal 1 or 2")
+    }
+}
