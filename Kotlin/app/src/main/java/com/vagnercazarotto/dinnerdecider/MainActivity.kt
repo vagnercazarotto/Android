67c4a7802c8d7ac8d267c3c5147251bc3b854030
vagnercazarotto <vagnercazarotto@gmail.com>
2018-07-01 14:44:11 +0200

    Just a simple review!!

--- Kotlin/app/src/main/java/com/vagnercazarotto/dinnerdecider/MainActivity.kt
+++ Kotlin/app/src/main/java/com/vagnercazarotto/dinnerdecider/MainActivity.kt
@@ -3,14 +3,30 @@ package com.vagnercazarotto.dinnerdecider
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val foodList = arrayListOf("Chines", "Hamburger", "Pizza", "McDonalds", "Barros Pizza")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        selectedFoodTxt.text = "Burger King"
+
+        decideButton.setOnClickListener {
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+            selectedFoodTxt.text = foodList[randomFood]
+            println(foodList[randomFood])
+        }
+
+        addFoodButton.setOnClickListener {
+            val newFood = addFoodText.text.toString()
+            foodList.add(newFood)
+            addFoodText.text.clear()
+        }
+
 
     }
 }
