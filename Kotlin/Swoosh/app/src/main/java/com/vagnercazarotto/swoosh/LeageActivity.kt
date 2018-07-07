980b5e6460511a2720713f016b28fd40118b92b0
vagnercazarotto <vagnercazarotto@gmail.com>
2018-07-07 14:49:20 +0200

    League Selection Layout

---
+++ Kotlin/Swoosh/app/src/main/java/com/vagnercazarotto/swoosh/LeageActivity.kt
@@ -0,0 +1,12 @@
+package com.vagnercazarotto.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LeageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_leage)
+    }
+}
