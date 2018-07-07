7e07c59c8c4f586688a9198d61edd8b91e3333f2
vagnercazarotto <vagnercazarotto@gmail.com>
2018-07-07 16:15:12 +0200

    Intents and Context

--- Kotlin/Swoosh/app/src/main/java/com/vagnercazarotto/swoosh/WelcomeActivity.kt
+++ Kotlin/Swoosh/app/src/main/java/com/vagnercazarotto/swoosh/WelcomeActivity.kt
@@ -2,11 +2,21 @@ package com.vagnercazarotto.swoosh
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getStartedBtn.setOnClickListener {
+            val leagueIntent = Intent(this, LeageActivity::class.java)
+            startActivity(leagueIntent)
+        }
+
     }
+
 }
+
