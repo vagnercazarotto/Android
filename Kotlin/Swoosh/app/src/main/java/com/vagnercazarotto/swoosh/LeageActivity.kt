51c34a338cfcd3bae8bcde4f70905f1fd9510b6a
vagnercazarotto <vagnercazarotto@gmail.com>
2018-07-07 18:12:12 +0200

    Skill Selection Layout

--- Kotlin/Swoosh/app/src/main/java/com/vagnercazarotto/swoosh/LeageActivity.kt
+++ Kotlin/Swoosh/app/src/main/java/com/vagnercazarotto/swoosh/LeageActivity.kt
@@ -1,12 +1,19 @@
 package com.vagnercazarotto.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LeageActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_leage)
+    }
+
+    fun leagueNextCliked(view: View){
+        val skillActivity = Intent(this, SkillActivity::class.java)
+        startActivity(skillActivity)
     }
 }
