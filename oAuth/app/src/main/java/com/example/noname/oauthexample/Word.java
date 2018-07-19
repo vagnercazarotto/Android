42da3ff7695801d6efade5b15dcf18afb0bdd01b
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-07-19 15:22:06 +0200

    Creating a Word Entity for use with ROOM

---
+++ oAuth/app/src/main/java/com/example/noname/oauthexample/Word.java
@@ -0,0 +1,23 @@
+package com.example.noname.oauthexample;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "word_table")
+public class Word {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+    public Word(@NonNull String word){
+        this.mWord = word;
+    }
+
+    public String getmWord() {
+        return this.mWord;
+    }
+}
