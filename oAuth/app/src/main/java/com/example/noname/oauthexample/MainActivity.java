54f57aee2a9445965fcd45d3c83862daaf42fe13
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-07-25 20:03:59 +0200

    Revert "Android Room with a View"
    This reverts commit d9222ea8d44f3de33895a1695a9b166c6afb5870.

--- oAuth/app/src/main/java/com/example/noname/oauthexample/MainActivity.java
+++ oAuth/app/src/main/java/com/example/noname/oauthexample/MainActivity.java
@@ -2,20 +2,12 @@ package com.example.noname.oauthexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_main);
-
-        RecyclerView recyclerView = findViewById(R.id.recyclerview);
-        final WordListAdapter adapter = new WordListAdapter(this);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
+        setContentView(R.layout.activity_main);
     }
 }
