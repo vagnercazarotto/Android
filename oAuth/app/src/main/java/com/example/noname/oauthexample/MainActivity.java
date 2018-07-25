e4f269e77f4456293a96d07574c9271d9e6614ea
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-07-25 20:03:58 +0200

    Revert "Populate the database"
    This reverts commit eeebf1b1fe7426f87d1929b1949f6f5c5d98b780.

--- oAuth/app/src/main/java/com/example/noname/oauthexample/MainActivity.java
+++ oAuth/app/src/main/java/com/example/noname/oauthexample/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.content_main);
 
         RecyclerView recyclerView = findViewById(R.id.recyclerview);
         final WordListAdapter adapter = new WordListAdapter(this);
--- oAuth/app/src/main/java/com/example/noname/oauthexample/WordRoomDatabase.java
+++ oAuth/app/src/main/java/com/example/noname/oauthexample/WordRoomDatabase.java
@@ -1,12 +1,9 @@
 package com.example.noname.oauthexample;
 
-import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
-import android.os.AsyncTask;
-import android.support.annotation.NonNull;
 
 @Database(entities = {Word.class}, version = 1, exportSchema = false)
 public abstract class WordRoomDatabase extends RoomDatabase {
@@ -23,7 +20,7 @@ public abstract class WordRoomDatabase extends RoomDatabase {
                 if(INSTANCE == null){
                     //create database here !!
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
-                            WordRoomDatabase.class, "word_database").addCallback(sRoomDatabaseCallback).build();
+                            WordRoomDatabase.class, "word_database").build();
                 }
             }
         }
@@ -31,35 +28,8 @@ public abstract class WordRoomDatabase extends RoomDatabase {
     }
 
 
-    private static RoomDatabase.Callback sRoomDatabaseCallback =
-            new RoomDatabase.Callback(){
-
-                @Override
-                public void onOpen (@NonNull SupportSQLiteDatabase db){
-                    super.onOpen(db);
-                    new PopulateDbAsync(INSTANCE).execute();
-                }
-            };
 
 
-    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
-
-        private final WordDao mDao;
-
-        PopulateDbAsync(WordRoomDatabase db) {
-            mDao = db.wordDao();
-        }
-
-        @Override
-        protected Void doInBackground(final Void... params) {
-            mDao.deleteAll();
-            Word word = new Word("Hello");
-            mDao.insert(word);
-            word = new Word("World");
-            mDao.insert(word);
-            return null;
-        }
-    }
 
 
 
