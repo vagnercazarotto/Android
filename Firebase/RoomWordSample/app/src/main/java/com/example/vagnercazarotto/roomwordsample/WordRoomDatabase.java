1b4ed261273513e9c801148b4dcdd70dd1d31a3a
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-07-25 21:46:04 +0200

    Clean and populate the database

--- Firebase/RoomWordSample/app/src/main/java/com/example/vagnercazarotto/roomwordsample/WordRoomDatabase.java
+++ Firebase/RoomWordSample/app/src/main/java/com/example/vagnercazarotto/roomwordsample/WordRoomDatabase.java
@@ -1,10 +1,13 @@
 package com.example.vagnercazarotto.roomwordsample;
 
 import android.arch.lifecycle.LiveData;
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 
 import java.util.List;
 
@@ -25,11 +28,51 @@ public abstract class WordRoomDatabase extends RoomDatabase {
                     // Create database here
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                             WordRoomDatabase.class, "word_database")
+                            .addCallback(sRoomDatabaseCallback)
                             .build();
                 }
             }
         }
         return INSTANCE;
     }
+
+
+    private static RoomDatabase.Callback sRoomDatabaseCallback =
+            new RoomDatabase.Callback(){
+
+                @Override
+                public void onOpen (@NonNull SupportSQLiteDatabase db){
+                    super.onOpen(db);
+                    new PopulateDbAsync(INSTANCE).execute();
+                }
+            };
+
+
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
+
+        private final WordDao mDao;
+
+        PopulateDbAsync(WordRoomDatabase db) {
+            mDao = db.wordDao();
+        }
+
+        @Override
+        protected Void doInBackground(final Void... params) {
+            mDao.deleteAll();
+            Word word = new Word("Hello");
+            mDao.insert(word);
+            word = new Word("World");
+            mDao.insert(word);
+            return null;
+        }
+    }
+
+
+
+
+
+
+
+
 
 }
