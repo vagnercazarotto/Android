8f618e6c4962552f4e37253ccbff930e9dfa2874
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-07-25 21:13:22 +0200

    Creating a DAO
    DAO handle the querys to java - obj

---
+++ Firebase/RoomWordSample/app/src/main/java/com/example/vagnercazarotto/roomwordsample/WordDao.java
@@ -0,0 +1,22 @@
+package com.example.vagnercazarotto.roomwordsample;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface WordDao {
+
+    //In the DAO (data access object), you specify SQL queries and associate them with method calls
+
+    @Insert
+    void insert(Word word);
+
+    @Query("DELETE FROM word_table")
+    void deleteAll();
+
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    List<Word> getAllWords();
+}
