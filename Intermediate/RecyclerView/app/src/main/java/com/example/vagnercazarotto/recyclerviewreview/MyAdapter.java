a1b0606f1c80fde81a1bcdb8033db4d70480ec1e
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-07-27 10:59:58 +0200

    Create a custom Adapter

---
+++ Intermediate/RecyclerView/app/src/main/java/com/example/vagnercazarotto/recyclerviewreview/MyAdapter.java
@@ -0,0 +1,57 @@
+package com.example.vagnercazarotto.recyclerviewreview;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private String[] mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        public  TextView mText;
+        public ViewHolder(TextView v){
+            super(v);
+            mText = v;
+        }
+    }
+
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MyAdapter(String[] myDataset){
+        mDataset = myDataset;
+    }
+
+
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        // create a new view
+        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.activity_main, parent, false);
+
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.mText.setText(mDataset[position]);
+    }
+
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+    
+
+}
