ec839d465e80510ff60ee706cf04265cf8c11409
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-07-27 10:39:53 +0200

    Should create a custom adapter for RecyclerView

--- Intermediate/RecyclerView/app/src/main/java/com/example/vagnercazarotto/recyclerviewreview/MainActivity.java
+++ Intermediate/RecyclerView/app/src/main/java/com/example/vagnercazarotto/recyclerviewreview/MainActivity.java
@@ -6,11 +6,17 @@ import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    final RecyclerView mRecyclerView = findViewById(R.id.myRecyclerView);
-    final LinearLayoutManager mLinearLayoutManager = new LinearLayoutManager(this,
-            LinearLayoutManager.VERTICAL, false);
+    // custom
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+
+
 
 
 
@@ -18,5 +24,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // instantiate
+        mRecyclerView = (RecyclerView) findViewById(R.id.myRecyclerView);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+//        // specify an adapter (see also next example)
+//        mAdapter = new MyAdapter(myDataset);
+//        mRecyclerView.setAdapter(mAdapter);
+
+
+
+
+
+
+
+
+
     }
+
+
+
+
+
+
+
+
 }
--- Intermediate/RecyclerView/app/src/main/java/com/example/vagnercazarotto/recyclerviewreview/MainModel.java
+++
@@ -1,4 +0,0 @@
-package com.example.vagnercazarotto.recyclerviewreview;
-
-public class MainModel {
-}
