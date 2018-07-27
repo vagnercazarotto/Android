5512a1c518321aa3343ee1c3638fd4fafb5a30e4
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-07-27 10:11:40 +0200

    Review some RecyclerView

--- Intermediate/RecyclerView/app/src/main/java/com/example/vagnercazarotto/recyclerviewreview/MainActivity.java
+++ Intermediate/RecyclerView/app/src/main/java/com/example/vagnercazarotto/recyclerviewreview/MainActivity.java
@@ -2,8 +2,17 @@ package com.example.vagnercazarotto.recyclerviewreview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    final RecyclerView mRecyclerView = findViewById(R.id.myRecyclerView);
+    final LinearLayoutManager mLinearLayoutManager = new LinearLayoutManager(this,
+            LinearLayoutManager.VERTICAL, false);
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ Intermediate/RecyclerView/app/src/main/java/com/example/vagnercazarotto/recyclerviewreview/MainModel.java
@@ -0,0 +1,4 @@
+package com.example.vagnercazarotto.recyclerviewreview;
+
+public class MainModel {
+}
