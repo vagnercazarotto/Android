51310d29f853a2cbd358d21ef0be6b4cbb9f8f31
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-08-08 18:10:01 +0200

    add Position class to the board game!

--- Java/ChessSystem/src/application/Program.java
+++ Java/ChessSystem/src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hi there!");
-	}
 
+		Position pos = new Position(3,4);
+		System.out.println(pos);
+		
+		
+	}
 }
