cee5c4cb5c374ebc6f1e65da33381a455392886a
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-08-10 17:34:41 +0200

    little improvement

--- Beginner/Retrofit/app/src/main/java/com/example/vagnercazarotto/retrofitguide/Contributor.java
+++ Beginner/Retrofit/app/src/main/java/com/example/vagnercazarotto/retrofitguide/Contributor.java
@@ -1,10 +1,8 @@
 package com.example.vagnercazarotto.retrofitguide;
 
 public class Contributor {
-
     public final String login;
     public final String contributions;
-
 
     public Contributor(String login, String contributions) {
         this.login = login;
--- Beginner/Retrofit/app/src/main/java/com/example/vagnercazarotto/retrofitguide/MainActivity.java
+++ Beginner/Retrofit/app/src/main/java/com/example/vagnercazarotto/retrofitguide/MainActivity.java
@@ -16,34 +16,44 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    public static final String API_URL = "https://api.github.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Create a very simple REST adapter which points the GitHub API.
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(API_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
+        // create an instance of our GitHubApi interface
+        GitHub gitHub = retrofit.create(GitHub.class);
+
+        // create a call instance for looking up Retrofit contributors
+        final Call<List<Contributor>> call = gitHub.contributions("square","retrofit");
 
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                ToDoInBackground mClass = new ToDoInBackground();
-                mClass.doInBackground();
+                // Fetch and print a list fo the contributos to the lib
+                List<Contributor> contributors = null;
+                try {
+                    contributors = call.execute().body();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                for (Contributor contributor : contributors){
+                    Log.d("TAGG","user: "+ contributor.login + " counter: " + contributor.contributions +"\n");
+                }
             }
 
         });
 
         thread.start();
 
-
-
-
     }
-
-
-
-
-
 
 }
