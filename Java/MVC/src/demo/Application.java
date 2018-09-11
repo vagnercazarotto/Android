aad1e966215d5b4d11ca7ed5a1026918ca0f1c67
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-09-11 12:33:28 +0200

    Base MVC

---
+++ Java/MVC/src/demo/Application.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("app");
+	}
+
+}
---
+++ Java/MVC/src/demo/view/View.java
@@ -0,0 +1,7 @@
+package demo.view;
+
+import javax.swing.JFrame;
+
+public class View extends JFrame {
+
+}
