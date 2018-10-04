df077aab057f3c2a4e82256fcbd23527334236c8
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-10-04 16:08:33 +0200

    Just trying the example for slide activity

--- OrganizzeClone/app/src/main/java/com/example/jamiltondamasceno/organizze/MainActivity.java
+++ OrganizzeClone/app/src/main/java/com/example/jamiltondamasceno/organizze/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.jamiltondamasceno.organizze;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
+
+public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide(new SimpleSlide.Builder()
+                .title("My Title")
+                .description("My description..")
+                .image(R.drawable.um)
+                .background(android.R.color.holo_orange_light)
+                .build());
+
+        addSlide(new SimpleSlide.Builder()
+                .title("My Title2")
+                .description("My description..2")
+                .image(R.drawable.dois)
+                .background(android.R.color.holo_orange_light)
+                .build());
+
+        addSlide(new SimpleSlide.Builder()
+                .title("My Title3")
+                .description("My description..3")
+                .image(R.drawable.tres)
+                .background(android.R.color.holo_orange_light)
+                .build());
+
+
     }
 }
