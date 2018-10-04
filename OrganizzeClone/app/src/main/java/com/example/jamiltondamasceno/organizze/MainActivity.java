e17f52a9232bfc946480071517e0f9a36b0c6b42
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-10-04 16:24:40 +0200

    Adding custom linear layout in the Slider library

--- OrganizzeClone/app/src/main/java/com/example/jamiltondamasceno/organizze/MainActivity.java
+++ OrganizzeClone/app/src/main/java/com/example/jamiltondamasceno/organizze/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.jamiltondamasceno.organizze;
 import android.os.Bundle;
 
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
 
 public class MainActivity extends IntroActivity {
@@ -11,29 +12,41 @@ public class MainActivity extends IntroActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setButtonBackVisible(false);
-        setButtonNextVisible(false);
 
-        addSlide(new SimpleSlide.Builder()
-                .title("My Title")
-                .description("My description..")
-                .image(R.drawable.um)
-                .background(android.R.color.holo_orange_light)
+        addSlide(new FragmentSlide.Builder()
+            .background(android.R.color.white)
+            .fragment(R.layout.intro_01)
+            .build());
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_02)
                 .build());
 
-        addSlide(new SimpleSlide.Builder()
-                .title("My Title2")
-                .description("My description..2")
-                .image(R.drawable.dois)
-                .background(android.R.color.holo_orange_light)
-                .build());
 
-        addSlide(new SimpleSlide.Builder()
-                .title("My Title3")
-                .description("My description..3")
-                .image(R.drawable.tres)
-                .background(android.R.color.holo_orange_light)
-                .build());
+//        setButtonBackVisible(false);
+//        setButtonNextVisible(false);
+//
+//        addSlide(new SimpleSlide.Builder()
+//                .title("My Title")
+//                .description("My description..")
+//                .image(R.drawable.um)
+//                .background(android.R.color.holo_orange_light)
+//                .build());
+//
+//        addSlide(new SimpleSlide.Builder()
+//                .title("My Title2")
+//                .description("My description..2")
+//                .image(R.drawable.dois)
+//                .background(android.R.color.holo_orange_light)
+//                .build());
+//
+//        addSlide(new SimpleSlide.Builder()
+//                .title("My Title3")
+//                .description("My description..3")
+//                .image(R.drawable.tres)
+//                .background(android.R.color.holo_orange_light)
+//                .build());
 
 
     }
