69f93cb625bef70695025edd7c1697cea518d558
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-10-05 16:54:13 +0200

    Validate and redirect user when is user logged in..

--- OrganizzeClone/app/src/main/java/com/example/jamiltondamasceno/organizze/activity/MainActivity.java
+++ OrganizzeClone/app/src/main/java/com/example/jamiltondamasceno/organizze/activity/MainActivity.java
@@ -6,6 +6,8 @@ import android.view.View;
 
 import com.example.jamiltondamasceno.organizze.activity.CadastroActivity;
 import com.example.jamiltondamasceno.organizze.activity.LoginActivity;
+import com.example.jamiltondamasceno.organizze.config.ConfigFirebase;
+import com.google.firebase.auth.FirebaseAuth;
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 import com.example.jamiltondamasceno.organizze.R;
@@ -13,10 +15,17 @@ import com.example.jamiltondamasceno.organizze.R;
 
 public class MainActivity extends IntroActivity {
 
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        verificarUsuarioLogado();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
 
         addSlide(new FragmentSlide.Builder()
             .background(android.R.color.white)
@@ -53,6 +62,17 @@ public class MainActivity extends IntroActivity {
 
     public void btnCadastrar(View view){
         startActivity(new Intent(this, CadastroActivity.class));
+    }
+
+    public void verificarUsuarioLogado(){
+        firebaseAuth = ConfigFirebase.getFirebaseAuth();
+        if(firebaseAuth.getCurrentUser() != null){
+            abrirTelaPrincipal();
+        }
+    }
+
+    public void abrirTelaPrincipal(){
+        startActivity(new Intent(this, PrincipalActivity.class));
     }
 
 
