fd5e0295db743ffdf607b8d81aa0f69dcdc76349
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-10-05 16:00:09 +0200

    Added firebase login service.

--- OrganizzeClone/app/src/main/java/com/example/jamiltondamasceno/organizze/activity/LoginActivity.java
+++ OrganizzeClone/app/src/main/java/com/example/jamiltondamasceno/organizze/activity/LoginActivity.java
@@ -1,16 +1,79 @@
 package com.example.jamiltondamasceno.organizze.activity;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.jamiltondamasceno.organizze.R;
+import com.example.jamiltondamasceno.organizze.config.ConfigFirebase;
+import com.example.jamiltondamasceno.organizze.model.Usuario;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText campoEmail, campoSenha;
+    private Button botaoEntrar;
+    private Usuario usuario;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        campoEmail = findViewById(R.id.editEmail);
+        campoSenha = findViewById(R.id.editSenha);
+        botaoEntrar = findViewById(R.id.buttonEntrar);
+
+        botaoEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String textoEmail = campoEmail.getText().toString();
+                String textoSenha = campoSenha.getText().toString();
+
+                if(!textoEmail.isEmpty()){
+                    if(!textoSenha.isEmpty()){
+                        usuario = new Usuario();
+                        usuario.setEmail(textoEmail);
+                        usuario.setSenha(textoSenha);
+                        validarLogin();
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Preencha o senha..", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(LoginActivity.this, "Preencha o e-mail..", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
+
+
+    public void validarLogin(){
+        firebaseAuth = ConfigFirebase.getFirebaseAuth();
+        firebaseAuth.signInWithEmailAndPassword(usuario.getEmail(),usuario.getSenha())
+        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(LoginActivity.this, "Login Successful.", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Erro ao fazer loguin.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
+
 }
