40a2b34008cb3cd0f39c6b013f0e45d899977541
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-10-08 18:18:07 +0200

    Added Floating action menu e button to the organizze clone.

--- OrganizzeClone/app/src/main/java/com/example/jamiltondamasceno/organizze/activity/PrincipalActivity.java
+++ OrganizzeClone/app/src/main/java/com/example/jamiltondamasceno/organizze/activity/PrincipalActivity.java
@@ -18,14 +18,18 @@ public class PrincipalActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
+
+
+    public void adicionarDespesa(View view){
+
+    }
+
+    public void adicionarReceita(View view){
+
+    }
+
+
+
 
 }
