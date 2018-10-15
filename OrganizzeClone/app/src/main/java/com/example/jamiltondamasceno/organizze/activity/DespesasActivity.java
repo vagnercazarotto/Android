0c151cb3e7dc369dde6302874a0322a456ed5863
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-10-15 17:32:55 +0200

    Saving the expenses

--- OrganizzeClone/app/src/main/java/com/example/jamiltondamasceno/organizze/activity/DespesasActivity.java
+++ OrganizzeClone/app/src/main/java/com/example/jamiltondamasceno/organizze/activity/DespesasActivity.java
@@ -3,15 +3,18 @@ package com.example.jamiltondamasceno.organizze.activity;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 import com.example.jamiltondamasceno.organizze.R;
 import com.example.jamiltondamasceno.organizze.helper.DataCustom;
+import com.example.jamiltondamasceno.organizze.model.Movimentacao;
 
 public class DespesasActivity extends AppCompatActivity {
 
     private TextInputEditText campoData, campoCategoria, campoDescricao;
     private EditText campoValor;
+    private Movimentacao movimentacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,4 +29,18 @@ public class DespesasActivity extends AppCompatActivity {
 
         campoData.setText(DataCustom.dataAtual());
     }
+
+    public void salvarDespesa(View view){
+        movimentacao = new Movimentacao();
+        String data = campoData.getText().toString();
+        movimentacao.setValor(Double.parseDouble(campoValor.getText().toString()));
+        movimentacao.setCategoria(campoCategoria.getText().toString());
+        movimentacao.setDescricao(campoDescricao.getText().toString());
+        movimentacao.setData(data);
+        movimentacao.setTipo("d");
+
+        movimentacao.salvar(data);
+    }
+
+
 }
---
+++ OrganizzeClone/app/src/main/java/com/example/jamiltondamasceno/organizze/model/Movimentacao.java
@@ -0,0 +1,78 @@
+package com.example.jamiltondamasceno.organizze.model;
+
+import android.provider.ContactsContract;
+
+import com.example.jamiltondamasceno.organizze.config.ConfigFirebase;
+import com.example.jamiltondamasceno.organizze.helper.Base64Custom;
+import com.example.jamiltondamasceno.organizze.helper.DataCustom;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Movimentacao {
+
+    private String data;
+    private String categoria;
+    private String descricao;
+    private String tipo;
+    private double valor;
+
+    public Movimentacao() {
+    }
+
+    public void salvar(String dataEscolhida){
+
+        FirebaseAuth firebase = ConfigFirebase.getFirebaseAuth();
+        String idUsuario = Base64Custom.codificarBase64(firebase.getCurrentUser().getEmail());
+        String mesAno = DataCustom.mesAnoDataEscolhida(dataEscolhida);
+
+        DatabaseReference databaseReference = ConfigFirebase.getFirebase();
+        databaseReference.child("movimentacao")
+                .child(idUsuario)
+                .child(mesAno)
+                .push()
+                .setValue(this);
+    }
+
+
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
