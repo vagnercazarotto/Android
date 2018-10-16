9700f3c9d3739d61e86c277ad3a9393928759a6b
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-10-16 20:17:30 +0200

    Add and config a toolbar !!

--- OrganizzeClone/app/src/main/java/com/example/jamiltondamasceno/organizze/activity/PrincipalActivity.java
+++ OrganizzeClone/app/src/main/java/com/example/jamiltondamasceno/organizze/activity/PrincipalActivity.java
@@ -4,10 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.jamiltondamasceno.organizze.R;
+import com.example.jamiltondamasceno.organizze.config.ConfigFirebase;
+import com.google.firebase.auth.FirebaseAuth;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
@@ -18,11 +22,14 @@ public class PrincipalActivity extends AppCompatActivity {
 
     private MaterialCalendarView calendarView;
     private TextView textoSaudacao, textoSaldo;
+    private FirebaseAuth auth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Organizze");
         setSupportActionBar(toolbar);
 
 
@@ -30,8 +37,6 @@ public class PrincipalActivity extends AppCompatActivity {
         textoSaudacao = findViewById(R.id.textSaudacao);
         calendarView = findViewById(R.id.calendarView);
         configuraCalendarView();
-
-
 
     }
 
@@ -47,6 +52,26 @@ public class PrincipalActivity extends AppCompatActivity {
         });
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menuSair :
+                auth = ConfigFirebase.getFirebaseAuth();
+                auth.signOut();
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     public void adicionarDespesa(View view){
         startActivity(new Intent(this, DespesasActivity.class));
