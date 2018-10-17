402e6ddb74023ce9da392f342401611e74be343a
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-10-17 12:29:38 +0200

    Recovery the resume from firebase

--- OrganizzeClone/app/src/main/java/com/example/jamiltondamasceno/organizze/activity/PrincipalActivity.java
+++ OrganizzeClone/app/src/main/java/com/example/jamiltondamasceno/organizze/activity/PrincipalActivity.java
@@ -11,18 +11,29 @@ import android.widget.TextView;
 
 import com.example.jamiltondamasceno.organizze.R;
 import com.example.jamiltondamasceno.organizze.config.ConfigFirebase;
+import com.example.jamiltondamasceno.organizze.helper.Base64Custom;
+import com.example.jamiltondamasceno.organizze.model.Usuario;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
+import java.text.DecimalFormat;
 import java.util.regex.MatchResult;
 
 public class PrincipalActivity extends AppCompatActivity {
 
     private MaterialCalendarView calendarView;
     private TextView textoSaudacao, textoSaldo;
-    private FirebaseAuth auth;
+    private DatabaseReference firebaseRef = ConfigFirebase.getFirebase();
+    private FirebaseAuth firebaseAuth = ConfigFirebase.getFirebaseAuth();
+    private Double despesaTotal = 0.0;
+    private Double receitaTotal = 0.0;
+    private Double resumoUsuario = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +47,9 @@ public class PrincipalActivity extends AppCompatActivity {
         textoSaldo = findViewById(R.id.textSaldo);
         textoSaudacao = findViewById(R.id.textSaudacao);
         calendarView = findViewById(R.id.calendarView);
+        
         configuraCalendarView();
+        recuperaResumo();
 
     }
 
@@ -52,6 +65,36 @@ public class PrincipalActivity extends AppCompatActivity {
         });
     }
 
+    public void recuperaResumo(){
+
+        String emailUsuario = firebaseAuth.getCurrentUser().getEmail();
+        String idUsuario = Base64Custom.codificarBase64(emailUsuario);
+        DatabaseReference usuarioRef = firebaseRef.child("usuarios").child(idUsuario);
+
+        usuarioRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Usuario usuario = dataSnapshot.getValue(Usuario.class);
+
+                despesaTotal = usuario.getDespesaTotal();
+                receitaTotal = usuario.getReceitaTotal();
+                resumoUsuario = receitaTotal - despesaTotal;
+
+                DecimalFormat decimalFormat = new DecimalFormat("0.##");
+                String resultadoFormatado = decimalFormat.format(resumoUsuario);
+
+                textoSaudacao.setText("Olà, " + usuario.getName());
+                textoSaldo.setText(resultadoFormatado);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -63,8 +106,7 @@ public class PrincipalActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case R.id.menuSair :
-                auth = ConfigFirebase.getFirebaseAuth();
-                auth.signOut();
+                firebaseAuth.signOut();
                 startActivity(new Intent(this, MainActivity.class));
                 finish();
                 break;
