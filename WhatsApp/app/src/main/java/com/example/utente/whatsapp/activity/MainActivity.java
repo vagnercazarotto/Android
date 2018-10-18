16f7cf151a8734077aa9ea014635e1977cc950d3
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-10-18 18:28:13 +0200

    Add a menu to sign out the user.

--- WhatsApp/app/src/main/java/com/example/utente/whatsapp/activity/MainActivity.java
+++ WhatsApp/app/src/main/java/com/example/utente/whatsapp/activity/MainActivity.java
@@ -3,18 +3,62 @@ package com.example.utente.whatsapp.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Switch;
 
 import com.example.utente.whatsapp.R;
+import com.example.utente.whatsapp.config.ConfiguracaoFirebase;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        auth = ConfiguracaoFirebase.getFirebaseAuth();
+
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
         toolbar.setTitle("WhatsApp");
         setSupportActionBar(toolbar);
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menuSair :
+                deslogarUsuario();
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void deslogarUsuario(){
+
+        try {
+            auth.signOut();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
