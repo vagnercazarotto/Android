cff1f56b7f8a8b409859930247075a147d6a9084
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-10-18 17:50:27 +0200

    Add a toolbar to the main layout

--- WhatsApp/app/src/main/java/com/example/utente/whatsapp/activity/MainActivity.java
+++ WhatsApp/app/src/main/java/com/example/utente/whatsapp/activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.utente.whatsapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.example.utente.whatsapp.R;
 
@@ -11,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.setTitle("WhatsApp");
+        setSupportActionBar(toolbar);
     }
 }
