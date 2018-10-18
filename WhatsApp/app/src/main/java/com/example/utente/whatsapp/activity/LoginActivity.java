34ade54af725fbbe6e9b0539f47f767cde6d62fe
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-10-18 15:46:04 +0200

    Add Login feature for users

--- WhatsApp/app/src/main/java/com/example/utente/whatsapp/activity/LoginActivity.java
+++ WhatsApp/app/src/main/java/com/example/utente/whatsapp/activity/LoginActivity.java
@@ -1,21 +1,101 @@
 package com.example.utente.whatsapp.activity;
 
 import android.content.Intent;
+import android.icu.util.ValueIterator;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.utente.whatsapp.R;
+import com.example.utente.whatsapp.config.ConfiguracaoFirebase;
+import com.example.utente.whatsapp.model.Usuario;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextInputEditText campoEmail, campoSenha;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        campoEmail = findViewById(R.id.editLoginEmail);
+        campoSenha = findViewById(R.id.editLoginSenha);
+
+        auth = ConfiguracaoFirebase.getFirebaseAuth();
+
+
     }
 
-    public void abrirTelaCadastro(View view){
+    public void abrirTelaPrincipal() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void logarUsuario(Usuario usuario) {
+        auth.signInWithEmailAndPassword(usuario.getEmail(), usuario.getSenha())
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            abrirTelaPrincipal();
+                        } else {
+                            String excesao = "";
+                            try {
+                                throw task.getException();
+                            } catch (FirebaseAuthWeakPasswordException e) {
+                                excesao = "Digite uma senha mais forte";
+                            } catch (FirebaseAuthUserCollisionException e) {
+                                excesao = "Esta conta ja existe..";
+                            } catch (FirebaseAuthInvalidCredentialsException e) {
+                                excesao = "Por favor digite um e-mail valido!!";
+                            } catch (Exception e) {
+                                excesao = "Erro ao cadastrar usuario: " + e.getMessage();
+                                e.printStackTrace();
+                            }
+                            Toast.makeText(LoginActivity.this, excesao, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    public void validarAutenticacaoUsuario(View view) {
+
+        String email = campoEmail.getText().toString();
+        String senha = campoEmail.getText().toString();
+
+
+        if (!email.isEmpty()) {
+            if (!senha.isEmpty()) {
+
+                Usuario usuario = new Usuario();
+                usuario.setEmail(email);
+                usuario.setSenha(senha);
+
+                logarUsuario(usuario);
+
+            } else {
+                Toast.makeText(LoginActivity.this, "Preencha o senha!", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(LoginActivity.this, "Preencha o email!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    public void abrirTelaCadastro(View view) {
         Intent intent = new Intent(LoginActivity.this, CadastroActivity.class);
         startActivity(intent);
     }
