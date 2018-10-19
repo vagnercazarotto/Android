03144c6a300185fdd251f6e8451204eaa6899e75
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-10-19 16:40:39 +0200

    Config the intent for the camera activity ( take a picture.. )

--- WhatsApp/app/src/main/java/com/example/utente/whatsapp/activity/ConfiguracoesActivity.java
+++ WhatsApp/app/src/main/java/com/example/utente/whatsapp/activity/ConfiguracoesActivity.java
@@ -2,12 +2,16 @@ package com.example.utente.whatsapp.activity;
 
 import android.Manifest;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.example.utente.whatsapp.R;
 import com.example.utente.whatsapp.helper.Permissao;
@@ -18,11 +22,17 @@ public class ConfiguracoesActivity extends AppCompatActivity {
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.CAMERA
     };
+    private ImageButton imageButtonCamera, imageButtonGaleria;
+    private static final int SELECAO_CAMERA = 100;
+    private static final int SELECAO_GALERIA = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracoes);
+
+        imageButtonCamera = findViewById(R.id.imageButtonCamera);
+        imageButtonGaleria = findViewById(R.id.imageButtonGaleria);
 
         //validade the permissions
         Permissao.validarPermissoes(permissoesNecessarias, this, 1);
@@ -32,6 +42,18 @@ public class ConfiguracoesActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        imageButtonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if(i.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(i,SELECAO_CAMERA);
+                }
+
+            }
+        });
 
     }
 
