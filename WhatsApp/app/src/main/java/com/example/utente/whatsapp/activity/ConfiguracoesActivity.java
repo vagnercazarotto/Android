4e01b7d729151895e943a51549c038962cb0684d
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-10-19 15:37:48 +0200

    Add permissions request for api > 23 in th helper module.

--- WhatsApp/app/src/main/java/com/example/utente/whatsapp/activity/ConfiguracoesActivity.java
+++ WhatsApp/app/src/main/java/com/example/utente/whatsapp/activity/ConfiguracoesActivity.java
@@ -1,17 +1,27 @@
 package com.example.utente.whatsapp.activity;
 
+import android.Manifest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
 import com.example.utente.whatsapp.R;
+import com.example.utente.whatsapp.helper.Permissao;
 
 public class ConfiguracoesActivity extends AppCompatActivity {
+
+    private String[] permissoesNecessarias = new String[]{
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracoes);
+
+        //validade the permissions
+        Permissao.validarPermissoes(permissoesNecessarias,this,1);
 
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
         toolbar.setTitle("Configuracoes");
