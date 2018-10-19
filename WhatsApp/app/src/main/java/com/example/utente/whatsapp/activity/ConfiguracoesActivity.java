3a809c110448fb04249959ab6272b65d5f4582cf
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-10-19 16:57:39 +0200

    Ok, recovering the images from camera or local storage and setting up as a profile picture.

--- WhatsApp/app/src/main/java/com/example/utente/whatsapp/activity/ConfiguracoesActivity.java
+++ WhatsApp/app/src/main/java/com/example/utente/whatsapp/activity/ConfiguracoesActivity.java
@@ -4,8 +4,11 @@ import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,6 +19,8 @@ import android.widget.ImageButton;
 import com.example.utente.whatsapp.R;
 import com.example.utente.whatsapp.helper.Permissao;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class ConfiguracoesActivity extends AppCompatActivity {
 
     private String[] permissoesNecessarias = new String[]{
@@ -25,6 +30,7 @@ public class ConfiguracoesActivity extends AppCompatActivity {
     private ImageButton imageButtonCamera, imageButtonGaleria;
     private static final int SELECAO_CAMERA = 100;
     private static final int SELECAO_GALERIA = 200;
+    private CircleImageView circleImageViewPerfil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +39,7 @@ public class ConfiguracoesActivity extends AppCompatActivity {
 
         imageButtonCamera = findViewById(R.id.imageButtonCamera);
         imageButtonGaleria = findViewById(R.id.imageButtonGaleria);
+        circleImageViewPerfil = findViewById(R.id.circleImageViewPerfil);
 
         //validade the permissions
         Permissao.validarPermissoes(permissoesNecessarias, this, 1);
@@ -48,15 +55,51 @@ public class ConfiguracoesActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if(i.resolveActivity(getPackageManager()) != null) {
-                    startActivityForResult(i,SELECAO_CAMERA);
+                if (i.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(i, SELECAO_CAMERA);
                 }
+            }
+        });
 
+
+        imageButtonGaleria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                if (i.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(i, SELECAO_GALERIA);
+                }
             }
         });
 
     }
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            Bitmap imagem = null;
+
+            try {
+                switch (requestCode) {
+                    case SELECAO_CAMERA:
+                        imagem = (Bitmap) data.getExtras().get("data");
+                        break;
+                    case SELECAO_GALERIA:
+                        Uri localImagemSelecionada = data.getData();
+                        imagem = MediaStore.Images.Media.getBitmap(getContentResolver(), localImagemSelecionada);
+                        break;
+                }
+                if(imagem != null) {
+                    circleImageViewPerfil.setImageBitmap(imagem);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
