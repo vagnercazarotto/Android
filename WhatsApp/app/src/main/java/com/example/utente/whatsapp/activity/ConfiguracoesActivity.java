113a5e2cd78516a33f8b9c5474b2b7db05b5810e
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-10-19 15:53:21 +0200

    Ok, Add a permission checker module and set up an Alert dialog for the config activity..

--- WhatsApp/app/src/main/java/com/example/utente/whatsapp/activity/ConfiguracoesActivity.java
+++ WhatsApp/app/src/main/java/com/example/utente/whatsapp/activity/ConfiguracoesActivity.java
@@ -1,6 +1,10 @@
 package com.example.utente.whatsapp.activity;
 
 import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -21,7 +25,7 @@ public class ConfiguracoesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_configuracoes);
 
         //validade the permissions
-        Permissao.validarPermissoes(permissoesNecessarias,this,1);
+        Permissao.validarPermissoes(permissoesNecessarias, this, 1);
 
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
         toolbar.setTitle("Configuracoes");
@@ -29,5 +33,35 @@ public class ConfiguracoesActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // discover if any permission was not granted
+        for (int permissaoResultado : grantResults) {
+            if (permissaoResultado == PackageManager.PERMISSION_DENIED) {
+                alertaValidacaoPermissao();
+            }
+        }
+
+    }
+
+    private void alertaValidacaoPermissao() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissao Negada");
+        builder.setMessage("Para utilizar o app é preciso aceitar as permissoes..");
+        builder.setCancelable(false);
+        builder.setPositiveButton("COnfirmar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
