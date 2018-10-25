228e1615df23d81b6c5c85f40eba5acf69ce1664
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-10-25 15:18:47 +0200

    Saving the chat message in firebase organized by user room.

--- WhatsApp/app/src/main/java/com/example/utente/whatsapp/activity/ChatActivity.java
+++ WhatsApp/app/src/main/java/com/example/utente/whatsapp/activity/ChatActivity.java
@@ -4,11 +4,19 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.utente.whatsapp.R;
+import com.example.utente.whatsapp.config.ConfiguracaoFirebase;
+import com.example.utente.whatsapp.helper.Base64Custom;
+import com.example.utente.whatsapp.helper.UsuarioFirebase;
+import com.example.utente.whatsapp.model.Mensagem;
 import com.example.utente.whatsapp.model.Usuario;
+import com.google.firebase.database.DatabaseReference;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -17,6 +25,11 @@ public class ChatActivity extends AppCompatActivity {
     private TextView textViewNome;
     private CircleImageView circleImageViewFoto;
     private Usuario usuarioDestinatario;
+    private EditText editMensagem;
+
+    //id users sender and receiver
+    private String idUsuarioRemetente;
+    private String idUsuarioDestinatario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +44,10 @@ public class ChatActivity extends AppCompatActivity {
         //init config
         textViewNome = findViewById(R.id.textViewNomeChat);
         circleImageViewFoto = findViewById(R.id.circleImageFoto);
+        editMensagem = findViewById(R.id.editMensagem);
+
+        // recover data from sender
+        idUsuarioRemetente = UsuarioFirebase.getIdentificadorUsuario();
 
         //recover the user destinatary user data
         Bundle bundle = getIntent().getExtras();
@@ -49,7 +66,37 @@ public class ChatActivity extends AppCompatActivity {
             }
         }
 
+        //recover data from destinatary
+        idUsuarioDestinatario = Base64Custom.codificarBase64(usuarioDestinatario.getEmail());
+
 
     }
+
+
+    public void enviarMensagem(View view) {
+
+        String textoMensagem = editMensagem.getText().toString();
+        if (!textoMensagem.isEmpty()) {
+            Mensagem mensagem = new Mensagem();
+            mensagem.setIdUsuario(idUsuarioRemetente);
+            mensagem.setMensagem(textoMensagem);
+
+            salvarMensagem(idUsuarioRemetente, idUsuarioDestinatario, mensagem);
+        } else {
+            Toast.makeText(ChatActivity.this, "Digite uma mensagem para enviar!", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+    private void salvarMensagem(String idRemetente, String idDestinatario, Mensagem msg) {
+        DatabaseReference database = ConfiguracaoFirebase.getFirebaseDatabase();
+        DatabaseReference mensagemRef = database.child("mensagens");
+
+        mensagemRef.child(idRemetente).child(idDestinatario).push().setValue(msg);
+
+        editMensagem.setText("");
+    }
+
 
 }
