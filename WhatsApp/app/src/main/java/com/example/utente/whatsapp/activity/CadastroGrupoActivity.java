b74a8c308e8cb1cdcadcdb5adef6b591e18fbada
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-11-08 18:12:32 +0100

    Build a interface for the cadastro grupo activity and dependencies

--- WhatsApp/app/src/main/java/com/example/utente/whatsapp/activity/CadastroGrupoActivity.java
+++ WhatsApp/app/src/main/java/com/example/utente/whatsapp/activity/CadastroGrupoActivity.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.utente.whatsapp.R;
+import com.example.utente.whatsapp.adapter.GrupoSelecionadoAdapter;
 import com.example.utente.whatsapp.model.Usuario;
 
 import java.util.ArrayList;
@@ -17,6 +20,10 @@ import java.util.List;
 public class CadastroGrupoActivity extends AppCompatActivity {
 
     private List<Usuario> listaMembrosSelecionados = new ArrayList<>();
+    private TextView textTotalParticipantes;
+    private GrupoSelecionadoAdapter grupoSelecionadoAdapter;
+    private RecyclerView recyclerMenbrosSelecionados;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +32,9 @@ public class CadastroGrupoActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //initial configs
+        textTotalParticipantes = findViewById(R.id.textTotalParticipantes);
+        recyclerMenbrosSelecionados = findViewById(R.id.recyclerMembrosGrupo);
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -41,8 +51,19 @@ public class CadastroGrupoActivity extends AppCompatActivity {
         if(getIntent().getExtras() != null) {
             List<Usuario> membros = (List<Usuario>) getIntent().getExtras().getSerializable("membros");
             listaMembrosSelecionados.addAll(membros);
+
+            textTotalParticipantes.setText("Total: " + listaMembrosSelecionados.size());
         }
 
+        //config the recyclerView
+        grupoSelecionadoAdapter = new GrupoSelecionadoAdapter(listaMembrosSelecionados, getApplicationContext());
+
+        RecyclerView.LayoutManager layoutManagerHorizontal = new LinearLayoutManager(
+                getApplicationContext(), LinearLayoutManager.HORIZONTAL, false);
+
+        recyclerMenbrosSelecionados.setLayoutManager(layoutManagerHorizontal);
+        recyclerMenbrosSelecionados.setHasFixedSize(true);
+        recyclerMenbrosSelecionados.setAdapter(grupoSelecionadoAdapter);
 
 
 
