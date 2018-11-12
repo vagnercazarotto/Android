e66246e996fff4a538ac50def044c76755ed0170
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-11-12 10:58:57 +0100

    wrong way to start a thread

--- Advanced/Threads/app/src/main/java/com/example/utente/threads/MainActivity.java
+++ Advanced/Threads/app/src/main/java/com/example/utente/threads/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.utente.threads;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void startThread(View view){
+        for(int i = 0; i <= 15; i++){
+            Log.d("Thread", "contador:  " +i);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void finishThread(View view){ }
+
+
+
+
 }
