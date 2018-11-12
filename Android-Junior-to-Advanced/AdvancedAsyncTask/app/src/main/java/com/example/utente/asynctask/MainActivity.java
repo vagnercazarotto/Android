0a4137ff983be9b48cb1ad305b959e98aedb83b3
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-11-12 17:48:15 +0100

    Cleaning the project structure and added Java BackEnd

---
+++ Android-Junior-to-Advanced/AdvancedAsyncTask/app/src/main/java/com/example/utente/asynctask/MainActivity.java
@@ -0,0 +1,99 @@
+package com.example.utente.asynctask;
+
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button botaoRecuperar;
+    private TextView textoResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botaoRecuperar = findViewById(R.id.buttonAsync);
+        textoResultado = findViewById(R.id.textView);
+
+        botaoRecuperar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyTask myTask = new MyTask();
+                String url = "https://blockchain.info/ticker";
+                String urlCEP = "https://viacep.com.br/ws/01001000/json";
+                myTask.execute(urlCEP);
+            }
+        });
+
+
+
+
+
+
+    }
+
+
+
+
+    class MyTask extends AsyncTask<String,Void,String>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(String resultado) {
+            super.onPostExecute(resultado);
+            textoResultado.setText(resultado);
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String stringURL = strings[0];
+            InputStream inputStream = null;
+            InputStreamReader inputStreamReader = null;
+            StringBuffer buffer = null;
+
+            try {
+                URL url = new URL(stringURL);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                inputStream = connection.getInputStream();
+                inputStreamReader = new InputStreamReader(inputStream);
+
+                BufferedReader reader = new BufferedReader(inputStreamReader);
+                String linha = "";
+                buffer = new StringBuffer();
+                while ((linha = reader.readLine()) != null){
+                    buffer.append(linha);
+                }
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return buffer.toString();
+        }
+    }
+
+
+
+}
