90aec9c0dc3c820b4d4606676c338473cfeaf2c6
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-11-12 15:36:55 +0100

    Async tasks progress bar..

--- AdvancedAsyncTask/app/src/main/java/com/example/utente/asynctask/MainActivity.java
+++ AdvancedAsyncTask/app/src/main/java/com/example/utente/asynctask/MainActivity.java
@@ -1,13 +1,73 @@
 package com.example.utente.asynctask;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progressBar2);
     }
+
+
+    public void startAsyncBtn(View view) {
+        MyAsyncTask myAsyncTask = new MyAsyncTask();
+        myAsyncTask.execute(10);
+    }
+
+    // 1 param
+    // 2 type of progress
+    // 3 return
+
+    class MyAsyncTask extends AsyncTask<Integer, Integer, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String doInBackground(Integer... integers) {
+
+            int numero = integers[0];
+            for(int i=0; i <numero; i++){
+                publishProgress(i*10);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            return "Finalizado";
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            progressBar.setProgress(values[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+            progressBar.setProgress(0);
+            progressBar.setVisibility(View.INVISIBLE);
+        }
+
+    }
+
 }
