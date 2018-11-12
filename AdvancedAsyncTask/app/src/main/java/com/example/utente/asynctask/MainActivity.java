227959b2d8106431c0847e538d999d10f19b880b
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-11-12 17:05:02 +0100

    Building a http request in the background ( testing some thread features )

--- AdvancedAsyncTask/app/src/main/java/com/example/utente/asynctask/MainActivity.java
+++ AdvancedAsyncTask/app/src/main/java/com/example/utente/asynctask/MainActivity.java
@@ -1,73 +1,82 @@
 package com.example.utente.asynctask;
 
+
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ProgressBar;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ProgressBar progressBar;
+    private Button botaoRecuperar;
+    private TextView textoResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        progressBar = findViewById(R.id.progressBar2);
+        botaoRecuperar = findViewById(R.id.buttonAsync);
+        textoResultado = findViewById(R.id.textView);
+
+        botaoRecuperar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyTask myTask = new MyTask();
+                String url = "https://blockchain.info/ticker";
+                myTask.execute(url);
+            }
+        });
+
+
+
+
+
+
     }
 
 
-    public void startAsyncBtn(View view) {
-        MyAsyncTask myAsyncTask = new MyAsyncTask();
-        myAsyncTask.execute(10);
-    }
 
-    // 1 param
-    // 2 type of progress
-    // 3 return
 
-    class MyAsyncTask extends AsyncTask<Integer, Integer, String> {
+    class MyTask extends AsyncTask<String,Void,String>{
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            progressBar.setVisibility(View.VISIBLE);
         }
 
         @Override
-        protected String doInBackground(Integer... integers) {
+        protected void onPostExecute(String resultado) {
+            super.onPostExecute(resultado);
+            textoResultado.setText(resultado);
+        }
 
-            int numero = integers[0];
-            for(int i=0; i <numero; i++){
-                publishProgress(i*10);
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+        @Override
+        protected String doInBackground(String... strings) {
+            String stringURL = strings[0];
 
+            try {
+                URL url = new URL(stringURL);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                InputStream inputStream = connection.getInputStream();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-
-            return "Finalizado";
+            return null;
         }
-
-        @Override
-        protected void onProgressUpdate(Integer... values) {
-            super.onProgressUpdate(values);
-            progressBar.setProgress(values[0]);
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
-            progressBar.setProgress(0);
-            progressBar.setVisibility(View.INVISIBLE);
-        }
-
     }
+
+
 
 }
