299ffd2af8f474c9b430f9c93ebc902da2e7ecda
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-11-13 16:11:49 +0100

    Getting a user from database

--- BackEnd-Java/STS/base/src/aplicacao/Programa.java
+++ BackEnd-Java/STS/base/src/aplicacao/Programa.java
@@ -10,23 +10,16 @@ public class Programa {
 	
 	public static void main(String[] args) { 
 		
-		Pessoa p1 = new Pessoa(null,"carlos", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(null,"carlos 2 ", "carlos2@gmail.com");
-		Pessoa p3 = new Pessoa(null,"carlos 3", "carlos3@gmail.com");
+
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("base-JPA");
 		EntityManager em = emf.createEntityManager();
 		
-		em.getTransaction().begin();
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-		em.getTransaction().commit();
-		System.out.println("OK, done");
+		Pessoa p = em.find(Pessoa.class,2);
 		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		System.out.println(p.toString());
+		em.close();
+		emf.close();
 		
 	}
 	
