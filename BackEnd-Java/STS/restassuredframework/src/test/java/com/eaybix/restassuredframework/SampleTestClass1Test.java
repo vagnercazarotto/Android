c29934c216c18639f52b38487d57acf487d6e8af
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-12-06 12:17:49 +0100

    Added a Maven project to integrate and run ons Jenkins

---
+++ BackEnd-Java/STS/restassuredframework/src/test/java/com/eaybix/restassuredframework/SampleTestClass1Test.java
@@ -0,0 +1,17 @@
+package com.eaybix.restassuredframework;
+
+import org.junit.Test;
+
+public class SampleTestClass1Test {
+
+	@Test	
+	public void firstTest() { 
+		System.out.print("Just a teste 1 ");
+	}
+	
+	@Test
+	public void secondTest() { 
+		System.out.print("Just a teste 2");
+	}
+	
+}
---
+++ BackEnd-Java/STS/restassuredframework/src/test/java/com/eaybix/restassuredframework/SampleTestClass2Test.java
@@ -0,0 +1,17 @@
+package com.eaybix.restassuredframework;
+
+import org.junit.Test;
+
+public class SampleTestClass2Test {
+
+	@Test	
+	public void thirdTest() { 
+		System.out.print("Just a test 3");
+	}
+	
+	@Test
+	public void fourthTest() { 
+		System.out.print("Just a test 4");
+	}
+	
+}
