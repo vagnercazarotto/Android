134a3f32582a8f3bb8b56559bacb9060c2429015
Vagner Cazarotto <vagnercazarotto@gmail.com>
2019-01-15 17:21:02 +0100

    update Facade

--- DesignPatterns/Facade/src/main.java
+++ DesignPatterns/Facade/src/main.java
@@ -3,6 +3,16 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		ScheduleServer scheduleServer = new ScheduleServer();
+		ScheduleServerFacade facadeServer = new ScheduleServerFacade(scheduleServer);
+		
+		facadeServer.startServer();
+		System.out.println("Start working......");
+		System.out.println("After work done.........");
+		facadeServer.stopServer();
+		
+		
 		System.out.println("ok");
 	}
 
