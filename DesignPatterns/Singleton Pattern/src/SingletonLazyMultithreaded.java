73fd946a90c494c8a5884c2e315d048dc8dc279c
Vagner Cazarotto <vagnercazarotto@gmail.com>
2019-02-05 17:43:59 +0100

    Singleton for multi-threaded environment.

---
+++ DesignPatterns/Singleton Pattern/src/SingletonLazyMultithreaded.java
@@ -0,0 +1,15 @@
+
+public class SingletonLazyMultithreaded {
+	
+	private static SingletonLazyMultithreaded sc = null;
+	
+	private SingletonLazyMultithreaded(){}
+	
+	public static synchronized SingletonLazyMultithreaded getInstance() { 
+		if(sc == null) { 
+			sc = new SingletonLazyMultithreaded();	
+		}
+		return sc;
+	}
+
+}
